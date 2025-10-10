8c7241799c2074028dc02cd05430b31c3fe4c270
Andrew Kille <68079167+apkille@users.noreply.github.com>
2025-10-09 22:17:37 -0700

    Add homodyne interface (#74)
    * add homodyne interface
    
    * stylistic changes to homodyne.jl file
    
    * add homodyne tests
    
    * Bump actions/checkout from 4 to 5 (#75)
    
    * Add SymplecticMatrices.jl as a dependency, replacing SymplecticFactorizations.jl (#78)
    
    * add SympMatrices as dep/rm SympFactorizations
    
    * up changelog and bump project.toml to 1.3.2-dev
    
    * Bump dawidd6/action-download-artifact from 9 to 11 (#69)
    
    Bumps [dawidd6/action-download-artifact](https://github.com/dawidd6/action-download-artifact) from 9 to 11.
    - [Release notes](https://github.com/dawidd6/action-download-artifact/releases)
    - [Commits](https://github.com/dawidd6/action-download-artifact/compare/v9...v11)
    
    ---
    updated-dependencies:
    - dependency-name: dawidd6/action-download-artifact
      dependency-version: '11'
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Update README.md
    
    * Update README.md
    
    * Bump peter-evans/create-or-update-comment from 4 to 5 (#80)
    
    Bumps [peter-evans/create-or-update-comment](https://github.com/peter-evans/create-or-update-comment) from 4 to 5.
    - [Release notes](https://github.com/peter-evans/create-or-update-comment/releases)
    - [Commits](https://github.com/peter-evans/create-or-update-comment/compare/v4...v5)
    
    ---
    updated-dependencies:
    - dependency-name: peter-evans/create-or-update-comment
      dependency-version: '5'
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Define `QuantumInterface.embed` for the `GaussianState` type (#81)
    
    * `embed` for `GaussianState`
    
    * update changelog
    
    * make doctest not random
    
    * remove static array tests
    
    * fix ptrace test
    
    * another silly doctest error
    
    * add homodyne measurement tests
    
    * fix test errors
    
    * use StableRNGs for random measurements
    
    * fix more tests and add Random.jl to project.toml
    
    * rm unstable rng tests
    
    * fix even more random tests
    
    * fix doctest
    
    * another test fix, insert tired face
    
    * rm Random pkgs from test project.toml
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- test/test_measurements.jl
+++ test/test_measurements.jl
@@ -1,14 +1,13 @@
 @testitem "Measurements" begin
     using Gabs
     using StaticArrays
-    using LinearAlgebra: det, I
+    using LinearAlgebra: det, I, cholesky, Symmetric
 
     @testset "generaldyne" begin
 
         qpairbasis, qblockbasis = QuadPairBasis(1), QuadBlockBasis(1)
         for basis in [qpairbasis, qblockbasis]
             vac = vacuumstate(basis)
-            @test_throws ArgumentError generaldyne(vac, [1, 2])
             @test_throws ArgumentError generaldyne(vac ⊗ vac, [1], proj = zeros(4, 4))
             @test_throws ArgumentError generaldyne(vac ⊗ vac, [1], proj = GaussianState(basis ⊕ basis, zeros(4), zeros(4, 4)))
             vacs = vac ⊗ vac ⊗ vac ⊗ vac
@@ -66,5 +65,93 @@
         @test size(rand(Generaldyne, rs_qpair, [1, 3, 5], shots = 10)) == (6, 10)
         @test size(rand(Generaldyne, rs_qblock, [1, 3, 5], shots = 10)) == (6, 10)
 
+    end
+
+    @testset "homodyne" begin
+
+        @testset "homodyne" begin
+            qpairbasis, qblockbasis = QuadPairBasis(1), QuadBlockBasis(1)
+        
+            for basis in (qpairbasis, qblockbasis)
+                vac = vacuumstate(basis)
+                @test_throws ArgumentError homodyne(vac, [1, 2], [0.0])
+                @test_throws ArgumentError homodyne(vac, [1], [0.0, π/2])
+            end
+        
+            # simple test case: measuring 1 mode of 2-mode state
+            for basis in (QuadPairBasis(2), QuadBlockBasis(2))
+                st = vacuumstate(basis) ⊗ vacuumstate(basis)
+                M = homodyne(st, [1], [0.0])
+                @test M isa Homodyne
+                @test M.result isa Vector
+                @test M.state isa GaussianState
+        
+                result, state = M
+                @test result == M.result
+                @test state == M.state
+        
+                # check if measured mode is replaced with vacuum
+                @test isapprox(state.mean[1:2], zeros(2), atol=1e-12)
+                @test isapprox(state.covar[1:2, 1:2], Matrix{Float64}(I,2,2), atol=1e-12)
+            end
+        
+            st = squeezedstate(QuadPairBasis(4), 0.5, π/2)
+            st_block = changebasis(QuadBlockBasis, st)
+            indices = [2, 4]
+            angles = [0.0, π/2]
+            @test size(rand(Homodyne, st, indices, angles, shots=10)) == (4, 10)
+            @test size(rand(Homodyne, st_block, indices, angles, shots=7)) == (4, 7)
+        
+            indices = [1, 2]
+            rs_qpair = randstate(QuadPairBasis(4))
+            rs_qblock = changebasis(QuadBlockBasis, rs_qpair)
+            M_qpair = homodyne(rs_qpair, indices, [0.0, π/2])
+            M_qblock = homodyne(rs_qblock, indices, [0.0, π/2])
+        
+            # extract analytical conditional update
+            xA, xB, VA, VB, VAB = Gabs._part_state(rs_qpair, indices)
+            B = copy(VB)
+            # apply infinite squeezing (0.0 and π/2)
+            θs = [0.0, π/2]
+            for i in 1:2
+                θ = θs[i]
+                sq = 1e-12
+                ct, st = cos(θ), sin(θ)
+                B[2i-1,2i-1] += ct^2 * sq + st^2 / sq
+                B[2i-1,2i]   += ct * st * (sq - 1 / sq)
+                B[2i,2i-1]   += ct * st * (sq - 1 / sq)
+                B[2i,2i]     += st^2 * sq + ct^2 / sq
+            end
+            L = cholesky(Symmetric(B)).L
+            resultmean = L * randn(4) + xB
+            xA′ = xA .+ VAB * (inv(B) * (resultmean - xB))
+            VA′ = VA .- VAB * (inv(B) * transpose(VAB))
+        
+            nm = 4
+            out_mean = zeros(2nm)
+            notindices = setdiff(1:nm, indices)
+            for i in eachindex(notindices)
+                idx = notindices[i]
+                out_mean[2idx-1:2idx] .= @view(xA′[2i-1:2i])
+            end
+            out_covar = Matrix{Float64}(I, 2nm, 2nm)
+            for i in eachindex(notindices), j in i:length(notindices)
+                idx, jdx = notindices[i], notindices[j]
+                out_covar[2idx-1:2idx, 2jdx-1:2jdx] .= @view(VA′[2i-1:2i, 2j-1:2j])
+                out_covar[2jdx-1:2jdx, 2idx-1:2idx] .= @view(VA′[2j-1:2j, 2i-1:2i])
+            end
+            expected_state = GaussianState(QuadPairBasis(nm), out_mean, out_covar)
+            @test isapprox(M_qpair.state.covar, expected_state.covar, atol=1e-8)
+            @test isapprox(M_qblock.state.covar, changebasis(QuadBlockBasis, expected_state).covar, atol=1e-8)
+        
+            sstatic = vacuumstate(SVector{2}, SMatrix{2,2}, QuadPairBasis(1))
+            statestatic = sstatic ⊗ sstatic ⊗ sstatic ⊗ sstatic
+            hstatic = homodyne(statestatic, [2], [π/4])
+            @test (hstatic.state).mean isa SVector && (hstatic.state).covar isa SMatrix
+            @test isequal(hstatic.state.mean[1:2], zeros(2))
+
+            @test_throws ArgumentError rand(Homodyne, rs_qpair, collect(1:5), [0.0])
+            @test_throws ArgumentError rand(Homodyne, rs_qblock, collect(1:5), [π/2])
+        end        
     end
 end
