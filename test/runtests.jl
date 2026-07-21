e4d27c400d11653b366a3ad717974fa2d55b703d
Stefan Krastanov <stefan@krastanov.org>
2026-07-21 10:57:30 -0400

    Normalize JET and downgrade test routing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,14 @@
 using TestItemRunner
 
-if ARGS == ["jet"]
+const JET_PROJECT = normpath(joinpath(@__DIR__, "projects", "jet"))
+const test_args = isempty(ARGS) ? ["general"] : ARGS
+const JET_flag = length(test_args) == 1 && startswith(only(test_args), "jet")
+
+if JET_flag
+    @info "Activating the dedicated JET test environment." project=JET_PROJECT
     using Pkg
 
-    Pkg.activate(joinpath(@__DIR__, "projects", "jet"))
+    Pkg.activate(JET_PROJECT)
     Pkg.instantiate()
     include("jet_tests.jl")
 else
